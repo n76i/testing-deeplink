86125811a6ee0200fba6ccbf58de114be7b68295
Nguyen Cuong <cuonnvan@ftech.ai>
2022-08-03 13:42:34 +0700

    adsa

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.sendFile( __dirname + "/" + "fbang-deeplink.html" );
+    res.sendFile( __dirname + "/public/" + "fbang-deeplink.html" );
 })
 
-var server = app.listen(80, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
    var host = server.address().address
    var port = server.address().port
    
